204df3bfd36373ba619aa07c990f7fd320b0e480
ThieryMichel <thiery@marmelab.com>
2015-10-15 14:31:43 +0200

    enable cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import koa from 'koa';
+import cors from 'koa-cors';
 import controller from './lib/controller';
 const app = koa();
 
+app.use(cors({origin: '*', methods: ['GET'], headers: ['Content-Type']}));
 app.use(controller.routes());
 app.use(controller.allowedMethods());
 
