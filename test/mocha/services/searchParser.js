2005c0a0e3d0af0668ef5c397c6cfecd3fc48e46
ThieryMichel <thiery@marmelab.com>
2015-10-16 18:32:29 +0200

    add searchParser service

---
+++ test/mocha/services/searchParser.js
@@ -0,0 +1,315 @@
+'use strict';
+
+import searchParser, * as extractor from '../../../lib/services/searchParser';
+import aidsResult from '../../mock/controller/aidsResult.json';
+
+
+
+describe.only('searchParser', function () {
+
+    it('should extract relevant information from ebsco raw result', function () {
+        const result = aidsResult.SearchResult.Data.Records;
+        assert.deepEqual(JSON.parse(JSON.stringify(result.map(searchParser))), require('./parsedAidsResult.json'));
+    });
+
+    describe('.extractTitle', function () {
+        it('return title of given result', function* () {
+            const result = {
+                ResultId: 1,
+                RecordInfo: {
+                    BibRecord: {
+                        BibEntity: {
+                            Titles: [
+                                {
+                                    Type: 'other',
+                                    TitleFull: 'other title'
+                                },
+                                {
+                                    Type: 'main',
+                                    TitleFull: 'main title'
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+            assert.equal(extractor.extractTitle(result), 'main title');
+        });
+
+        it('return null if no title found', function* () {
+            const result = {
+                ResultId: 1,
+                RecordInfo: {
+                    BibRecord: {
+                    }
+                }
+            };
+            assert.equal(extractor.extractTitle(result), null);
+        });
+
+        it('return null if no main title found', function* () {
+            const result = {
+                ResultId: 1,
+                RecordInfo: {
+                    BibRecord: {
+                        BibEntity: {
+                            Titles: [
+                                {
+                                    Type: 'other',
+                                    TitleFull: 'other title'
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+            assert.equal(extractor.extractTitle(result), null);
+        });
+    });
+
+    describe('.extractAuthors', function () {
+        it('should return list of authors for result', function () {
+            const result = {
+                RecordInfo: {
+                    BibRecord: {
+                        BibRelationships: {
+                            HasContributorRelationships: [
+                                {
+                                    PersonEntity: {
+                                        Name: {
+                                            NameFull: 'Mary Curry'
+                                        }
+                                    }
+                                }, {
+                                    PersonEntity: {
+                                        Name: {
+                                            NameFull: 'Louis Pasteur'
+                                        }
+                                    }
+                                }, {
+                                    PersonEntity: {
+                                        Name: {
+                                            NameFull: 'Albert Newton'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+            assert.deepEqual(extractor.extractAuthors(result), ['Mary Curry', 'Louis Pasteur', 'Albert Newton']);
+        });
+
+        it('should return null if no author found', function () {
+            const result = {
+                RecordInfo: {
+                    BibRelationships: {}
+                }
+            };
+            assert.deepEqual(extractor.extractAuthors(result), null);
+        });
+    });
+
+    describe('extractPublicationDate', function () {
+
+        it ('should return publicationDate from result', function () {
+
+            const result = {
+                RecordInfo: {
+                    BibRecord: {
+                        BibRelationships:{
+                            IsPartOfRelationships:[{
+                                BibEntity:{
+                                    Dates: [
+                                        {
+                                            M: 12,
+                                            D: 1,
+                                            Y: 2015,
+                                            Type: 'published'
+                                        }
+                                    ]
+                                }
+                            }]
+                        }
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractPublicationDate(result), new Date('12/01/2015'));
+        });
+
+        it ('should return null if date is invalid', function () {
+            const result = {
+                RecordInfo: {
+                    BibRecord: {
+                        BibRelationships:{
+                            IsPartOfRelationships:[{
+                                BibEntity:{
+                                    Dates: [
+                                        {
+                                            M: 99,
+                                            D: 99,
+                                            Y: 2015,
+                                            Type: 'published'
+                                        }
+                                    ]
+                                }
+                            }]
+                        }
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractPublicationDate(result), null);
+        });
+
+        it ('should return null if no date is found', function () {
+            const result = {
+                RecordInfo: {
+                    BibRelationships:{
+                        IsPartOfRelationships:{
+                            BibEntity:{}
+                        }
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractPublicationDate(result), null);
+        });
+    });
+
+    describe('extractLanguages', function () {
+
+        it ('should return languages from result', function () {
+            const result = {
+                RecordInfo: {
+                    BibRecord:{
+                        BibEntity:{
+                            Languages: [
+                                {
+                                    Code: 'eng',
+                                    Text: 'English'
+                                }, {
+                                    Code: 'fra',
+                                    Text: 'French'
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractLanguages(result), ['English', 'French']);
+        });
+
+        it ('should return null if no language is found', function () {
+            const result = {
+                RecordInfo: {
+                    BibRecord:{
+                        BibEntity:{}
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractLanguages(result), null);
+        });
+    });
+
+    describe('extractDatabase', function () {
+
+        it ('should return database from result', function () {
+            const result = {
+                Header: {
+                    DbLabel: 'mysql?'
+                }
+            };
+
+            assert.equal(extractor.extractDatabase(result), 'mysql?');
+        });
+
+        it ('should return null if no database is found', function () {
+            const result = {
+                Header: {}
+            };
+
+            assert.isNull(extractor.extractDatabase(result));
+        });
+
+    });
+
+    describe('extracSubjects', function () {
+
+        it ('should return subject list from result', function () {
+            const result = {
+                RecordInfo: {
+                    BibRecord:{
+                        BibEntity:{
+                            Subjects: [
+                                {
+                                    SubjectFull: 'The fermi paradox'
+                                }, {
+                                    SubjectFull: `Hempel's ravens`
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+
+            assert.deepEqual(extractor.extractSubjects(result), [ 'The fermi paradox', `Hempel's ravens` ]);
+        });
+
+        it ('should return null if no subjects is found', function () {
+            const result = {
+                RecordInfo: {}
+            };
+
+            assert.isNull(extractor.extractSubjects(result));
+        });
+
+    });
+
+    describe('extractPublicationType', function () {
+
+        it ('should return publicationType from result', function () {
+            const result = {
+                Header: {
+                    PubType: 'Dissertation/ Thesis'
+                }
+            };
+
+            assert.equal(extractor.extractPublicationType(result), 'Dissertation/ Thesis');
+        });
+
+        it ('should return null if no subjects is found', function () {
+            const result = {
+                RecordInfo: {}
+            };
+
+            assert.isNull(extractor.extractPublicationType(result));
+        });
+
+    });
+
+    describe('extractNoticeLink', function () {
+
+        it ('should return noticeLink from result', function () {
+            const result = {
+                PLink: 'https://en.wikipedia.org/wiki/Fermi_paradox'
+            };
+
+            assert.equal(extractor.extractNoticeLink(result), 'https://en.wikipedia.org/wiki/Fermi_paradox');
+        });
+
+        it ('should return null if no noticeLink is found', function () {
+            const result = {
+                RecordInfo: {}
+            };
+
+            assert.isNull(extractor.extractNoticeLink(result));
+        });
+
+    });
+
+});
