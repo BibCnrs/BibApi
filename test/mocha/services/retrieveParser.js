eadd792faeb4910c893e7737a048d2176004d026
ThieryMichel <thiery@marmelab.com>
2015-11-18 09:47:57 +0100

    add label in retrieve result

--- test/mocha/services/retrieveParser.js
+++ test/mocha/services/retrieveParser.js
@@ -7,27 +7,28 @@ describe('retrieveParser', function () {
     it('should extract notice data from ebsco record', function () {
         const data = {
             Header: {
+                DbId: 'databaseId',
                 DbLabel: 'database name'
             },
             Items: [
-                { Name: 'a', Data: 'data for a' },
-                { Name: 'b', Data: 'data for b' },
-                { Name: 'c', Data: 'data for c' },
-                { SurName: 'd', Data: 'data for d' },
-                { Name: 'e', Datum: 'data for e' }
+                { Name: 'a', Label: 'label a', Data: 'data for a' },
+                { Name: 'b', Label: 'label b', Data: 'data for b' },
+                { Name: 'c', Label: 'label c', Data: 'data for c' },
+                { SurName: 'd', Label: 'label d', Data: 'data for d' },
+                { Name: 'e', Label: 'label e', Datum: 'data for e' }
             ]
         };
         assert.deepEqual(retrieveParser(data), [
-            {name: 'a', value: 'data for a'},
-            {name: 'b', value: 'data for b'},
-            {name: 'c', value: 'data for c'},
-            {name: 'db', value: 'database name'}
+            {name: 'a', label: 'label a', value: 'data for a'},
+            {name: 'b', label: 'label b', value: 'data for b'},
+            {name: 'c', label: 'label c', value: 'data for c'},
+            {name: 'db', label: 'database name', value: 'databaseId'}
         ]);
     });
 
     it ('should default db to undefined', function () {
         assert.deepEqual(retrieveParser({}), [{
-            name: 'db', value: undefined
+            name: 'db', label: undefined, value: undefined
         }]);
     });
 
