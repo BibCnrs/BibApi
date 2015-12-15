167698ad1b3a5881a2981670f760f627cf1338b8
ThieryMichel <thiery@marmelab.com>
2015-12-15 18:21:36 +0100

    add currentPage parameter to search route

--- test/mocha/services/ebscoEdsSearch.js
+++ test/mocha/services/ebscoEdsSearch.js
@@ -6,9 +6,10 @@ import aidsResult from '../../mock/controller/aidsResult.json';
 
 describe('ebscoEdsSearch', function () {
 
-    let receivedTerm, receivedLimiters, receivedToken;
+    let receivedTerm, receivedLimiters, receivedToken, receivedCurrentPage;
     beforeEach(function () {
         apiServer.router.post(`/edsapi/rest/Search`, function* (next) {
+            receivedCurrentPage = this.request.body.RetrievalCriteria.PageNumber;
             receivedTerm = this.request.body.SearchCriteria.Queries[0].Term;
             receivedLimiters = this.request.body.SearchCriteria.Limiters;
             receivedToken = this.request.header['x-sessiontoken'];
@@ -18,8 +19,9 @@ describe('ebscoEdsSearch', function () {
     });
 
     it('should send term, token, and limiters', function* () {
-        let result = yield ebscoEdsSearch('aids', { FT: 'y', DT1: '2015-01/2015-11'}, 'token-for-profile-vie');
+        let result = yield ebscoEdsSearch('aids', 10, { FT: 'y', DT1: '2015-01/2015-11'}, 'token-for-profile-vie');
         assert.equal(receivedTerm, 'aids');
+        assert.equal(receivedCurrentPage, 10);
         assert.deepEqual(receivedLimiters, [
             { Id: 'FT', Values: ['y'] },
             { Id: 'DT1', Values: ['2015-01/2015-11'] }
@@ -28,8 +30,17 @@ describe('ebscoEdsSearch', function () {
         assert.deepEqual(result, aidsResult);
     });
 
+    it('should default currentPage to 1 if none given', function* () {
+        let result = yield ebscoEdsSearch('aids', undefined, undefined, 'token-for-profile-vie');
+        assert.equal(receivedTerm, 'aids');
+        assert.equal(receivedCurrentPage, 1);
+        assert.deepEqual(receivedLimiters, []);
+        assert.equal(receivedToken, 'token-for-profile-vie');
+        assert.deepEqual(result, aidsResult);
+    });
+
     it('should default limiters to empty array if none given', function* () {
-        let result = yield ebscoEdsSearch('aids', undefined, 'token-for-profile-vie');
+        let result = yield ebscoEdsSearch('aids', 1, undefined, 'token-for-profile-vie');
         assert.equal(receivedTerm, 'aids');
         assert.deepEqual(receivedLimiters, []);
         assert.equal(receivedToken, 'token-for-profile-vie');
@@ -37,7 +48,7 @@ describe('ebscoEdsSearch', function () {
     });
 
     it('should ignore limiters that are not allowed', function* () {
-        let result = yield ebscoEdsSearch('aids', { FT: 'y', DT1: '2015-01/2015-11', disallowed: 'ignored'}, 'token-for-profile-vie');
+        let result = yield ebscoEdsSearch('aids', 1, { FT: 'y', DT1: '2015-01/2015-11', disallowed: 'ignored'}, 'token-for-profile-vie');
         assert.deepEqual(receivedLimiters, [
             { Id: 'FT', Values: ['y'] },
             { Id: 'DT1', Values: ['2015-01/2015-11'] }
