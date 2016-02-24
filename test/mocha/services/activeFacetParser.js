6c1d1b19105db82270934a1ec29c3e3d49c63ca6
ThieryMichel <thiery@marmelab.com>
2016-02-24 14:37:39 +0100

    add activeFacetParser service

---
+++ test/mocha/services/activeFacetParser.js
@@ -0,0 +1,88 @@
+import { parse, unparse } from '../../../lib/services/activeFacetParser';
+
+describe.only('activeFacetParser', function () {
+    describe('parse', function () {
+        it('should return a literal with [Id]: [Value]', function () {
+            assert.deepEqual(parse([
+                {
+                    FilterId: 1,
+                    FacetValues: [
+                        {
+                            Id: 'Language',
+                            Value: 'french'
+                        }
+                    ]
+                },
+                {
+                    FilterId: 2,
+                    FacetValues: [
+                        {
+                            Id: 'SourceType',
+                            Value: 'Non-Print Resources'
+                        }
+                    ]
+                }
+            ]), {
+                Language: [ 'french' ],
+                SourceType: [ 'Non-Print Resources' ]
+            });
+        });
+
+        it('should concatene several on the same Id', function () {
+            assert.deepEqual(parse([
+                {
+                    FilterId: 1,
+                    FacetValues: [
+                        {
+                            Id: 'Language',
+                            Value: 'french'
+                        }
+                    ]
+                },
+                {
+                    FilterId: 2,
+                    FacetValues: [
+                        {
+                            Id: 'Language',
+                            Value: 'english'
+                        }
+                    ]
+                }
+            ]), {
+                Language: [ 'french', 'english' ]
+            });
+        });
+    });
+
+    describe('unparse', function () {
+        it('should return rawFacetActiveFacets from parsed one', function () {
+            assert.deepEqual(unparse({
+                Language: [ 'french', 'english' ],
+                SourceType: [ 'Non-Print Resources' ]
+            }), [
+                {
+                    FilterId: 1,
+                    FacetValues: [
+                        {
+                            Id: 'Language',
+                            Value: 'french'
+                        }, {
+                            Id: 'Language',
+                            Value: 'english'
+                        }
+                    ]
+                },
+                {
+                    FilterId: 2,
+                    FacetValues: [
+                        {
+                            Id: 'SourceType',
+                            Value: 'Non-Print Resources'
+                        }
+                    ]
+                }
+            ]);
+        });
+    });
+
+});
