b0550e1e90725341ec63fa8af99161d0f91b6c17
ThieryMichel <thiery@marmelab.com>
2016-03-04 17:30:38 +0100

    use parseXML with parseItems

--- test/mocha/services/parseXML.js
+++ test/mocha/services/parseXML.js
@@ -1,6 +1,6 @@
 import parseXML, { extractSearchLink, extractIndice, extractValue, parseXMLLine } from '../../../lib/services/parseXML';
 
-describe.only('parseXML', function () {
+describe('parseXML', function () {
     it('should extract searchLink', function () {
         const authorField = '&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Chen+S%22&quot;&gt;Chen S&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. hebeicdc2013@sina.com.&lt;br /&gt;&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Zhao+H%22&quot;&gt;Zhao H&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. sunline6666@sina.com.&lt;br /&gt;&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Zhao+C%22&quot;&gt;Zhao C&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. zhaocuiying906@sina.com.';
 
@@ -49,6 +49,10 @@ describe.only('parseXML', function () {
     describe('extractValue', function () {
         it('should return trimed string after last tag', function () {
             assert.equal(extractValue('ah<searchLink fieldCode="AU">ouh</searchLink>oh<relatesTo attr="yes">oups</relatesTo> ; hello  '), 'hello');
+        });
+
+        it('should return trimed value if no tag', function () {
+            assert.equal(extractValue(' ; hello  '), 'hello');
         });
 
         it('should return empty string if no content after last tag', function () {
