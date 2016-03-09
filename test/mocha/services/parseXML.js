3f41d29c9f2d965d54413e97f81c1bd10ec0c756
ThieryMichel <thiery@marmelab.com>
2016-03-09 16:12:21 +0100

    refactor parseXML to use xml-flow

--- test/mocha/services/parseXML.js
+++ test/mocha/services/parseXML.js
@@ -1,168 +1,125 @@
-import parseXML, { extractSearchLink, extractIndice,  extractFirstValue, extractLastValue, parseXMLLine,  parseLabelValue } from '../../../lib/services/parseXML';
+import parseXML, {  parseXMLLine,  parseXMLObject, smartConcat } from '../../../lib/services/parseXML';
 
-describe('parseXML', function () {
-    it('should extract searchLink', function () {
+describe.only('parseXML', function () {
+    it('should extract searchLink', function* () {
         const authorField = '&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Chen+S%22&quot;&gt;Chen S&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. hebeicdc2013@sina.com.&lt;br /&gt;&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Zhao+H%22&quot;&gt;Zhao H&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. sunline6666@sina.com.&lt;br /&gt;&lt;searchLink fieldCode=&quot;AU&quot; term=&quot;%22Zhao+C%22&quot;&gt;Zhao C&lt;/searchLink&gt;; Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. zhaocuiying906@sina.com.';
 
-        assert.deepEqual(parseXML(authorField), [
-            {
-                searchable: [{
+        assert.deepEqual(yield parseXML(authorField), [
+            [
+                {
                     field: 'AU',
-                    indice: undefined,
                     term: '"Chen S"',
                     value: 'Chen S'
-                }],
-                firstValue: '',
-                lastValue: 'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. hebeicdc2013@sina.com.'
-            },
-            {
-                searchable: [{
+                },
+                'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. hebeicdc2013@sina.com.'
+            ],
+            [
+                {
                     field: 'AU',
-                    indice: undefined,
                     term: '"Zhao H"',
                     value: 'Zhao H'
-                }],
-                firstValue: '',
-                lastValue: 'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. sunline6666@sina.com.'
-            },
-            {
-                searchable: [{
+                },
+                'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. sunline6666@sina.com.'
+            ],
+            [
+                {
                     field: 'AU',
-                    indice: undefined,
                     term: '"Zhao C"',
                     value: 'Zhao C'
-                }],
-                firstValue: '',
-                lastValue: 'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. zhaocuiying906@sina.com.'
-            }
+                },
+                'Hebei Province Center for Disease Control and Prevention, 97 Huaian East Road, Yuhua District, Shijiazhuang, 050021, China. zhaocuiying906@sina.com.'
+            ]
         ]);
     });
 
-    describe('extractSearchLink', function () {
-        it('should return searchLink tag child as term and fieldCode property as field', function () {
-            assert.deepEqual(extractSearchLink('<searchLink fieldCode="AU" term="hello" >hello</searchLink><relatesTo>19</relatesTo>'), [
-                { term: 'hello', value: 'hello', field: 'AU', indice: '19' }
-            ]);
-            assert.deepEqual(extractSearchLink('<searchLink fieldCode="AU" term="hello" >hello</searchLink><superscript>7</superscript>'), [
-                { term: 'hello', value: 'hello', field: 'AU', indice: '7' }
-            ]);
+    describe('parseXMLObject', function () {
+        it('should return value if it is a string', function () {
+            assert.equal(parseXMLObject('hello'), 'hello');
         });
 
-        it('should return all searchLink if several ones', function () {
-            const value = '<searchLink fieldCode="MM" term="%22Disease+Outbreaks%22">Disease Outbreaks*</searchLink><relatesTo>5</relatesTo>/<searchLink fieldCode="MM" term="%22Disease+Outbreaks+prevention+%26+control%22">prevention & control</searchLink>';
-            assert.deepEqual(extractSearchLink(value), [
-                { term: '"Disease Outbreaks"', value: 'Disease Outbreaks*', field: 'MM', indice: '5' },
-                { term: '"Disease Outbreaks prevention & control"', value: 'prevention & control', field: 'MM', indice: undefined }
-            ]);
-        });
-
-        it('should return emptyArray if there is no searchLink tag', function () {
-            assert.deepEqual(extractSearchLink('<notsearchLink fieldCode="AU">hello</notsearchLink>'), []);
-        });
-    });
-
-    describe('extractIndice', function () {
-        it('should return RelatesTo tag child', function () {
-            assert.equal(extractIndice('<relatesTo attr="yes">hello</relatesTo>'), 'hello');
-        });
-
-        it('should return superscript tag child', function () {
-            assert.equal(extractIndice('<superscript attr="yes">hello</superscript>'), 'hello');
-        });
-
-        it('should return null if there is no RelatesTo tag', function () {
-            assert.isNull(extractIndice('<notRelatesTo>hello</notRelatesTo>'));
-        });
-    });
-
-    describe('extractLastValue', function () {
-        it('should return trimed string after last tag', function () {
-            assert.equal(extractLastValue('ah<searchLink fieldCode="AU">ouh</searchLink>oh<relatesTo attr="yes">oups</relatesTo> ; hello  '), 'hello');
-        });
-
-        it('should return trimed value if no tag', function () {
-            assert.equal(extractLastValue(' ; hello  '), 'hello');
-        });
-
-        it('should return empty string if no content after last tag', function () {
-            assert.equal(extractLastValue('ah<searchLink fieldCode="AU">ouh</searchLink>oh<relatesTo attr="yes">oups</relatesTo>'), '');
-        });
-    });
-
-    describe('extractFirstValue', function () {
-        it('should return trimed string before first tag', function () {
-            assert.equal(extractFirstValue('  ah ;<searchLink fieldCode="AU">ouh</searchLink>oh<relatesTo attr="yes">oups</relatesTo> ; hello  '), 'ah');
-        });
-
-        it('should return trimed value if no tag', function () {
-            assert.equal(extractFirstValue(' ; hello  '), 'hello');
-        });
-
-        it('should return empty string if no content before first tag', function () {
-            assert.equal(extractFirstValue('<searchLink fieldCode="AU">ouh</searchLink>oh<relatesTo attr="yes">oups</relatesTo> ; hello '), '');
-        });
-    });
-
-    describe('parseLabelValue', function () {
-        it('should convert <i>to label and following string to value', function () {
-            assert.deepEqual(parseLabelValue('<i>label</i>value'), {
-                'label': 'value'
-            });
-        });
-
-        it('should work with several <i>', function () {
-            assert.deepEqual(parseLabelValue('<i>label1</i>value1<i>label2</ i>value2<i>label3</i>value3'), {
-                label1: 'value1',
-                label2: 'value2',
-                label3: 'value3'
-            });
-        });
-
-        it('should work parse searchLinkValue too', function () {
-            assert.deepEqual(parseLabelValue('<i>label</i><searchLink fieldCode="TI" term="search me">search</searchLink>'), {
-                label: {
-                    firstValue: '',
-                    lastValue: '',
-                    searchable: [{
-                        field: 'TI',
-                        value: 'search',
-                        term: 'search me',
-                        indice: undefined
-                    }]
+        it('should return $attrs.term, $attrs.fieldcode as field and $test as value if $name is "searchlink"', function () {
+            assert.deepEqual(parseXMLObject({
+                $name: 'searchlink',
+                $text: 'search label',
+                $attrs: {
+                    term: 'search term',
+                    fieldcode: 'field code'
                 }
+            }), {
+                value: 'search label',
+                term: 'search term',
+                field: 'field code'
             });
+        });
+
+        it('should return {indice: $text} if $name is relatesto', function () {
+            assert.deepEqual(parseXMLObject({
+                $name: 'relatesto',
+                $text: 'indice value'
+            }), {
+                indice: 'indice value'
+            });
+        });
+
+        it('should return $text if $name is i', function () {
+            assert.deepEqual(parseXMLObject({
+                $name: 'i',
+                $text: 'tag value'
+            }), 'tag value');
+        });
+    });
+
+    describe('smartConcat', function () {
+        it('should add value to array', function () {
+            assert.deepEqual(smartConcat([{ item: 1}], 'new value'), [{ item: 1}, 'new value']);
+        });
+
+        it('should not add value to array if it is falsy', function () {
+            assert.deepEqual(smartConcat([{ item: 1}], null), [{ item: 1}]);
+            assert.deepEqual(smartConcat([{ item: 1}], undefined), [{ item: 1}]);
+            assert.deepEqual(smartConcat([{ item: 1}], ''), [{ item: 1}]);
+            assert.deepEqual(smartConcat([{ item: 1}], 0), [{ item: 1}]);
+        });
+
+        it('should join value with array last item if both are string', function () {
+            assert.deepEqual(smartConcat([{ item: 1}, 'join'], 'us'), [{ item: 1}, 'join us']);
         });
     });
 
     describe('parseXMLLine', function () {
-        it('should return object with value extracted from string', function() {
-            assert.deepEqual(parseXMLLine(' first value <searchLink fieldCode="field" term="term">searchable value</searchLink><relatesTo>indice</relatesTo> last value '), {
-                searchable: [{
-                    term: 'term',
-                    field: 'field',
-                    value: 'searchable value',
+        it('should return object with value extracted from string', function* () {
+            assert.deepEqual(
+                yield parseXMLLine(' first value <searchLink fieldCode="field" term="term">searchable value</searchLink><relatesTo>indice</relatesTo> last value '),
+                [
+                    'first value',
+                    {
+                        term: 'term',
+                        field: 'field',
+                        value: 'searchable value'
+                    },
+                    {
+                        indice: 'indice'
+                    },
+                    'last value'
+                ]
+            );
+        });
+
+        it('should return passed value if there is no tag', function* () {
+            assert.deepEqual(yield parseXMLLine(' value '), 'value');
+        });
+
+        it('should return lastValue and indice extracted from string if there is no searchLink tag but one relatesTo tag', function* () {
+            assert.deepEqual(yield parseXMLLine('<relatesTo>indice</relatesTo> after'), [
+                {
                     indice: 'indice'
-                }],
-                firstValue: 'first value',
-                lastValue: 'last value'
-            });
+                },
+                'after'
+            ]);
         });
 
-        it('should return lastValue extracted from string if there is no searchLink tag', function() {
-            assert.deepEqual(parseXMLLine(' value '), 'value');
-        });
-
-        it('should return lastValue and indice extracted from string if there is no searchLink tag but one relatesTo tag', function() {
-            assert.deepEqual(parseXMLLine('<relatesTo>indice</relatesTo> value '), {
-                lastValue: 'value',
-                indice: 'indice'
-            });
-        });
-
-        it('should use parseLabelValue if it start with <i>', function () {
-            assert.deepEqual(parseXMLLine('<i>label</i>value'), {
-                label: 'value'
-            });
+        it('should use parseLabelValue if it start with <i>', function* () {
+            assert.deepEqual(yield parseXMLLine('<i>label</i>value'), ['label value']);
         });
     });
 
