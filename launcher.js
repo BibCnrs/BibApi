69f7da7b170228e7b1f3a4d4b560d367d1a92b99
ThieryMichel <thiery@marmelab.com>
2016-06-24 14:13:34 +0200

    have the migration execute at server start

--- launcher.js
+++ launcher.js
@@ -3,10 +3,20 @@
 require('babel/register')({ blacklist: [ 'regenerator' ] });
 var config = require('config');
 
+var command = require('./lib/utils/command');
+var path =  require('path');
+
 var app = require('./server');
 
 if (!module.parent) {
-    app.listen(config.port);
+
+    command(path.join(__dirname, './node_modules/migrat/bin/migrat up'))
+    .then((result) => {
+        global.console.log(result);
+        app.listen(config.port);
+    }, (error) => {
+        global.console.error(error);
+    });
 
     console.log('Server listening on port ' + config.port);
     console.log('Press CTRL+C to stop server');
