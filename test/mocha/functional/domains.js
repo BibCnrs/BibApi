16b72d3dfcf515899dbb152b5870689188895c1b
ThieryMichel <thiery@marmelab.com>
2018-02-22 10:45:27 +0100

    Revert "Merge pull request #156 from BibCnrs/domains_gate"
    This reverts commit 900cac6877f2bf2e0697b0c8a113861174074680, reversing
    changes made to e1be21b6e805698da2e8605a1adbd377ac536057.

--- test/mocha/functional/domains.js
+++ test/mocha/functional/domains.js
@@ -1,22 +1,12 @@
-describe('GET /ebsco/domains', function () {
+describe('POST /ebsco/domains', function () {
     it('should return all ebsco domains', function* () {
         const domains = ['insb', 'inshs', 'in2p3', 'inc', 'reaxys'];
 
-        yield domains.map(
-            name => fixtureLoader.createCommunity({
-                name,
-                ebsco: name !== 'reaxys',
-                gate: `${name}.bib.cnrs.fr`
-            })
-        );
+        yield domains
+        .map(name => fixtureLoader.createCommunity({ name, ebsco: name !== 'reaxys' }));
 
         const response = yield request.get('/ebsco/domains');
-        assert.deepEqual(JSON.parse(response.body), [
-            { name: 'insb', gate: 'insb.bib.cnrs.fr' },
-            { name: 'inshs', gate: 'inshs.bib.cnrs.fr' },
-            { name: 'in2p3', gate: 'in2p3.bib.cnrs.fr' },
-            { name: 'inc', gate: 'inc.bib.cnrs.fr' },
-        ]);
+        assert.deepEqual(JSON.parse(response.body), ['insb', 'inshs', 'in2p3', 'inc']);
     });
 
 
