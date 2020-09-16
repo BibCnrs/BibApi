fe9edb13b28f3b33a45b983a81acbd171fa6c3ce
join <florence.join@inist.fr>
2020-09-16 15:14:05 +0200

    maj config docker pour ubuntu 18

--- config/verif.js
+++ config/verif.js
@@ -1,4 +1,4 @@
 module.exports = {
     bibadmin_host: 'https://bib.verif.bib.cnrs.fr/admin',
-    api_endpoint: 'https://bib-dev.inist.fr/api',
+    api_endpoint: 'https://bib.verif.bib.cnrs.fr/api',
 };
