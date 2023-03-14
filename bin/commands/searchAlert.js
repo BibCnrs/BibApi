67fd0d753c8b2460ef00bbe3a45d7f55e4020a24
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-03-14 09:54:11 +0100

    fix: Update data handling to avoid crash durring search

--- bin/commands/searchAlert.js
+++ bin/commands/searchAlert.js
@@ -91,6 +91,7 @@ function* main() {
                     last_results,
                     user_id,
                 }) {
+                    const parsed_last_results = JSON.parse(last_results);
                     try {
                         nbAlerteTested++;
                         alertLogger.info('alert for', {
@@ -147,18 +148,15 @@ function* main() {
 
                         const newRawRecords = getMissingResults(
                             fullResult,
-                            last_results,
+                            parsed_last_results,
                         );
                         if (!newRawRecords.length) {
                             alertLogger.info(
                                 'No new results (newRawRecords vide)',
                             );
-                            yield updateOne(
-                                { id },
-                                {
-                                    last_execution: new Date(),
-                                },
-                            );
+                            yield updateOne(id, {
+                                last_execution: new Date(),
+                            });
                             nbLastExeDateUpdated++;
                             return;
                         }
